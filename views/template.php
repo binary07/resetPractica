c78bb1dfcca78bd186bb80bb22b5a3550c20b5d8
binary07 <0001ethan@gmail.com>
2018-09-01 14:26:44 -0500

    esta rama pone color al parrafo

--- views/template.php
+++ views/template.php
@@ -5,6 +5,6 @@
 </head>
 <body>
 <p>Este es el contenido</p>
-<p>Este es un cambio realizado el en la rama nueva carcteristica</p>
+<p style="text-align: center;color: red; background: blue;">Este es un cambio realizado el en la rama nueva carcteristica</p>
 </body>
 </html>
